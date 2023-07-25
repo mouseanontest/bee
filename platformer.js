9ad266f62dd443f87dc4b35ce9d21486e0475da2
GadkariAn <gadkari.aneesh@gmail.com>
2023-07-24 22:33:19 -0500

    Added jumps+platforms

--- platformer.js
+++ platformer.js
@@ -1,6 +1,6 @@
-let player1=null
-let player2=null
-
+let player1
+let player2
+let camera
 
 var config = {
     type: Phaser.AUTO,
@@ -20,8 +20,9 @@ var config = {
 
 class Player extends Phaser.Physics.Arcade.Sprite{
     constructor(scene,x,y){
-        super(scene,x,y,'player')
+        super(scene,x,y,'bee')
         scene.add.existing(this)
+        this.setScale(.5)
         scene.physics.add.existing(this)
         this.setCollideWorldBounds(true)
         this.setGravityY(3000)
@@ -34,21 +35,34 @@ function preload(){
     this.load.image('background', 'images/background.png')
     this.load.image('pentagon', 'images/pentagon.png')
     this.load.image('bee', 'images/bee.png')
+    this.load.image('platform', 'images/platform.png')
 }
 
 function create(){
+    this.add.tileSprite(0, 0, game.scale.width, game.scale.height, 'background').setOrigin(0,0)
+    createPlatforms(this)
     player1=new Player(this, 100,100)
     player2=new Player(this,200,100)
+    this.physics.add.collider(player1, platforms);
+    this.physics.add.collider(player2, platforms);
     cursors = this.input.keyboard.createCursorKeys()
     keys = this.input.keyboard.addKeys('W, A, D')
-    keys.W.on('down', jump);
-    cursors.up.on('down', jump);
+    keys.W.on('down', jump)
+    cursors.up.on('down', jump)
+}
+
+function createPlatforms(scene){
+    platforms = scene.physics.add.staticGroup()
+    let basePlatform = platforms.create(game.scale.width/2, game.scale.height-30, 'platform');
+    basePlatform.setScale(3, 1).refreshBody()
+    // platforms.create(250, 350, 'platform')
+    // platforms.create(950, 500, 'platform') 
 }
 
 function update(){
     let maxSpd=350
-    let accel=40
-    let decel=45
+    let accel=80
+    let decel=70
 
     if (cursors.left.isDown){
         player2.setVelocityX(appr(accel, -1*maxSpd, player2.body.velocity.x))
@@ -71,9 +85,9 @@ function update(){
 
 function jump(event){
     if (keys.W.isDown && player1.body.touching.down){
-        player1.setVelocityY(800)
+        player1.setVelocityY(-1000)
     } else if (cursors.up.isDown && player2.body.touching.down){
-        player2.setVelocityY(-800)
+        player2.setVelocityY(-1000)
     }
 }
 
