e6b791e13c503deb1f887a7ab899a53a8fd0b188
josebalboa2007 <108709606+josebalboa2007@users.noreply.github.com>
2023-07-28 09:06:42 -0500

    Update mg.js

--- pg/mg.js
+++ pg/mg.js
@@ -8,47 +8,44 @@ const config = {
   },
 };
 
-// Create a new Phaser game
 const game = new Phaser.Game(config);
 
-// Global variables
+
 let card1 = null;
 let card2 = null;
 let cardsFlipped = 0;
-let currentPlayer = 'Player 1'; // Track the current player's turn
+let currentPlayer = 'Player 1'; 
 let player1Score = 0;
 let player2Score = 0;
 let scoreTextPlayer1;
 let scoreTextPlayer2;
-let winText; // New variable to store the win text
+let winText; 
 
-// Card images (replace with your own images)
 const cardImages = [
   'card1',
-  'card1', // Pair 1 (two cards with the same image key 'card1')
+  'card1', 
   'card2',
-  'card2', // Pair 2 (two cards with the same image key 'card2')
+  'card2', 
   'card3',
-  'card3', // Pair 3 (two cards with the same image key 'card3')
+  'card3', 
   'card4',
-  'card4', // Pair 4 (two cards with the same image key 'card4')
+  'card4', 
   'card5',
-  'card5', // Pair 5 (two cards with the same image key 'card5')
+  'card5', 
   'card6',
-  'card6', // Pair 6 (two cards with the same image key 'card6')
+  'card6', 
   'card7',
-  'card7', // Pair 7 (two cards with the same image key 'card7')
+  'card7',
   'card8',
-  'card8', // Pair 8 (two cards with the same image key 'card8')
+  'card8', 
 ];
 
-// Shuffle the card images
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-}
+}f
 
 function preload() {
   this.load.image('background', 'images/background.png');
@@ -59,8 +56,8 @@ function preload() {
   this.load.image('card4', 'images/beeofalltradeslogo.png');
   this.load.image('card5', 'images/playerBee.png');
   this.load.image('card6', 'images/beefilmStudiosLogo.png');
-  this.load.image('card7', 'images/beehive.png'); // Replace 'your_image7.png' with the correct filename for card 7
-  this.load.image('card8', 'images/honeyBottle.png'); // Replace 'your_image8.png' with the correct filename for card 8
+  this.load.image('card7', 'images/beehive.png'); 
+  this.load.image('card8', 'images/honeyBottle.png');
 
   for (const cardImage of cardImages) {
     this.load.image(cardImage, `images/${cardImage}.png`);
@@ -68,14 +65,11 @@ function preload() {
 }
 
 function create() {
-  // Add the background
   this.add.image(0, 0, 'background').setScale(1.1);
 
-  // Shuffle the card images
   shuffleArray(cardImages);
 
-  // Create the grid for the cards
-  const gridSize = 4; // 4x4 grid
+  const gridSize = 4; 
   const cardSize = 100;
   const spacing = 42;
   const xOffset = (config.width - gridSize * (cardSize + spacing)) / 2;
@@ -84,7 +78,6 @@ function create() {
   let cardx = 100;
   let cardy = 100;
 
-  // Create cards and add click event
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       let x = cardx + j * (cardSize + spacing);
@@ -92,8 +85,7 @@ function create() {
 
       const card = this.add.image(x, y, 'card-back').setInteractive().setScale(0.25);
       card.setData('flipped', false);
-      card.setData('imageKey', cardImages[i * gridSize + j]); // Assign the correct image key
-
+      card.setData('imageKey', cardImages[i * gridSize + j]); 
       card.on('pointerup', function () {
         if ((currentPlayer === 'Player 1' && this !== card2) || (currentPlayer === 'Player 2' && this !== card1)) {
           flipCard(this);
@@ -102,13 +94,10 @@ function create() {
     }
   }
 
-  // Create and display score text for Player 1
   scoreTextPlayer1 = this.add.text(config.width - 200, 265, 'Player 1 Score: 0', { font: '24px Arial', fill: '#ffffff' });
 
-  // Create and display score text for Player 2
   scoreTextPlayer2 = this.add.text(config.width - 200, 300, 'Player 2 Score: 0', { font: '24px Arial', fill: '#ffffff' });
 
-  // Create and display the win text, but hide it initially
   winText = this.add.text(config.width / 2, config.height / 2, '', { font: '48px Arial', fill: '#ffffff' }).setOrigin(0.5);
   winText.setVisible(false);
 }
@@ -151,21 +140,17 @@ function checkMatch() {
       card1 = null;
       card2 = null;
 
-      // Switch turns between players
       currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
 
-      // Check if all cards are flipped and display the winner
       checkWin.call(this);
     }, 1000);
   }
 }
 
-// Helper function to update score text on the screen for Player 1
 function updateScoreTextPlayer1(text) {
   scoreTextPlayer1.setText(text);
 }
 
-// Helper function to update score text on the screen for Player 2
 function updateScoreTextPlayer2(text) {
   scoreTextPlayer2.setText(text);
 }
